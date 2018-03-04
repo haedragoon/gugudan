cea238272225fea2eb46107f093389905e78e175
Jarl_Dragoon <Jarl_Dragoon@121.141.135.76>
2018-03-04 12:02:01 +0900

    initial commit, to have been made 2dan and 3dan

---
+++ src/Gugudan.java
@@ -0,0 +1,28 @@
+
+public class Gugudan {
+	public static void main(String[] args) {
+		System.out.println("2단");
+		System.out.println("2 X 1 = " +  2*1);
+		System.out.println("2 X 2 = " + 2*2);
+		System.out.println("2 X 3 = " + 2*3);
+		System.out.println("2 X 4 = " + 2*4);
+		System.out.println("2 X 5 = " + 2*5);
+		System.out.println("2 X 6 = " + 2*6);
+		System.out.println("2 X 7 = " + 2*7);
+		System.out.println("2 X 8 = " + 2*8);
+		System.out.println("2 X 9 = " + 2*9);
+		//3단
+		System.out.println("3단");
+		System.out.println("3 X 1 = " + 3*1);
+		System.out.println("3 X 2 = " + 3*2);
+		System.out.println("3 X 3 = " + 3*3);
+		System.out.println("3 X 4 = " + 3*4);
+		System.out.println("3 X 5 = " + 3*5);
+		System.out.println("3 X 6 = " + 3*6);
+		System.out.println("3 X 7 = " + 3*7);
+		System.out.println("3 X 8 = " + 3*8);
+		System.out.println("3 X 9 = " + 3*9);
+		
+	}
+
+}
